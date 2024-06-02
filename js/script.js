2e8bc240351dc7d14673ed04a3d0df00d49bddfd
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-02 20:02:27 +0200

    Dopo alcuni tentativi tra div e span capito che l'uso del template qui mi era necessario per come l'avevo pensata. Applicato v-for al template. Ciclando sui contatti in chatAttiva.messages Creato due  div con dentro  span con classi che mi permettono di dare l'effetto messaggistica. Al quale ho applicato un v-if e un v-else. La condizione che che se message.status è === 'received' viene visualizzato uno altimenti l'altro.

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ createApp({
                 name: 'Matteo',
                 avatar: './img/avatar_io.jpg',
             },
-            chatAttiva: 0,
+            chatAttiva: 3,
             
         }
     },
